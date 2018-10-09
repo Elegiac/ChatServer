f811b7edd9f0efcb62ddf58e94d6b4a54d983b69
don.zhang <don.zhang@yeahmobi.com>
2018-10-09 16:56:30 +0800

    change log

--- src/main/java/stu/demo/server/message/StringServerHandler.java
+++ src/main/java/stu/demo/server/message/StringServerHandler.java
@@ -6,6 +6,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import edu.demo.common.entity.User;
 import edu.demo.common.entity.action.BaseAction;
 import edu.demo.common.entity.action.Chat;
@@ -17,10 +20,13 @@ import io.netty.channel.SimpleChannelInboundHandler;
 
 public class StringServerHandler extends SimpleChannelInboundHandler<String> {
 
+	private static final Logger log = LoggerFactory.getLogger(StringServerHandler.class);
+
 	private static final Map<String, User> USER_MAP;
 
 	static {
-		List<User> userList = Arrays.asList(new User("1", "123", "123"), new User("2", "321", "321"),new User("3","124","124"));
+		List<User> userList = Arrays.asList(new User("1", "123", "123"), new User("2", "321", "321"),
+				new User("3", "124", "124"));
 
 		Map<String, User> userMap = new HashMap<String, User>();
 		for (User user : userList) {
@@ -33,7 +39,7 @@ public class StringServerHandler extends SimpleChannelInboundHandler<String> {
 	@Override
 	protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
 		// 收到消息直接打印输出
-		System.out.println(ctx.channel().remoteAddress() + " 消息 : " + msg);
+		log.info("{} 消息: {} ", ctx.channel().remoteAddress(), msg);
 
 		Map<String, Object> map = JsonUtil.jsonToMap(msg);
 
@@ -79,14 +85,14 @@ public class StringServerHandler extends SimpleChannelInboundHandler<String> {
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
 
-		System.out.println("RamoteAddress : " + ctx.channel().remoteAddress() + " active !");
+		log.info("RamoteAddress : {} active !", ctx.channel().remoteAddress());
 
 		super.channelActive(ctx);
 	}
 
 	@Override
 	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-		System.out.println("RamoteAddress : " + ctx.channel().remoteAddress() + " inactive !");
+		log.info("RamoteAddress : {} inactive !", ctx.channel().remoteAddress());
 		MessageManager.userOffGroup(ctx);
 		super.channelInactive(ctx);
 	}
